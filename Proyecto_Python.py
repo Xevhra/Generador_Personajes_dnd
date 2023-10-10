bdcb61e1ed04526cb5d3cdbf9d3b884894de4203
Xevhra <garciarodriguezalvaro12@gmail.com>
2023-10-10 10:34:51 +0200

    Cambio

--- Proyecto_Python.py
+++ Proyecto_Python.py
@@ -2,10 +2,6 @@
 # repositorios de peliculas
 
 import requests
-import json
 
-print("¿Qué libro estas buscando?")
-ISBN = input()
-
-res = requests.get(f"https://www.goodreads.com/book/review_counts.json", params={"key": "gmaVsowZsITzZGWKQjQ3sQ", "isbns": "{ISBN}"})
+res = requests.get(f"https://www.goodreads.com/book/review_counts.json")
 print(res)
