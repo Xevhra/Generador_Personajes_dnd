eaffaed692d7bb62123745c26218713a83f34c8e
Xevhra <garciarodriguezalvaro12@gmail.com>
2023-10-10 09:47:29 +0200

    Se ha añadido un entorno virtual

--- Proyecto_Python.py
+++ Proyecto_Python.py
@@ -4,6 +4,4 @@
 import requests
 import json
 res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "gmaVsowZsITzZGWKQjQ3sQ", "isbns": "0743269268"})
-print(res.text)
-data=res.json()
-print(data)
+print(res)
