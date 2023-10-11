07e0c0bca2cb5acc56864a018e49cab1d3336175
Xevhra <garciarodriguezalvaro12@gmail.com>
2023-10-11 09:07:40 +0200

    Estructura del programa

--- Proyecto_Python.py
+++ Proyecto_Python.py
@@ -1,7 +1,34 @@
-# Desde esta linea importaremos el JSON de varias
-# repositorios de peliculas
+# La idea es realizar una aplicación que te monte un personaje de DnD
+# en base a tu selección
 
 import requests
 
-res = requests.get(f"https://www.goodreads.com/book/review_counts.json")
-print(res)
+# Aqui importaremos la api de la clase que seleccionas
+
+urlclases = 'https://www.dnd5eapi.co/api/classes'
+urlrazas = 'https://www.dnd5eapi.co/api/races'
+
+clasesdata = requests.get(urlclases)
+razasdata = requests.get(urlrazas)
+
+# Comencemos por el nombre
+
+print('Bienvenido a la creacion de personaje')
+print('Elije el nombre del mismo: ')
+
+nombre = input()
+
+# A continuación, definiremos la raza
+if razasdata.status_code == 200:
+    data = razasdata.json()
+    for e in data['results']:
+        print(e['index'])
+
+
+# Aqui elegiremos la clase
+
+#if data.status_code == 200:
+#   data = data.json()
+#    for e in data['results']:
+#        resultado = e + e['index']
+#        print (resultado)
