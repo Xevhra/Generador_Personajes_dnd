9431addc4511fabdc72cf19fba6dcea48e41fe6f
Xevhra <garciarodriguezalvaro12@gmail.com>
2023-10-11 10:35:46 +0200

    actualización esqueleto programa

--- Proyecto_Python.py
+++ Proyecto_Python.py
@@ -1,7 +1,10 @@
+#!/usr/bin/python3Al
+
 # La idea es realizar una aplicación que te monte un personaje de DnD
 # en base a tu selección
 
 import requests
+import json
 
 # Aqui importaremos la api de la clase que seleccionas
 
@@ -19,16 +22,50 @@ print('Elije el nombre del mismo: ')
 nombre = input()
 
 # A continuación, definiremos la raza
+print('****************************')
+
 if razasdata.status_code == 200:
-    data = razasdata.json()
-    for e in data['results']:
-        print(e['index'])
+    dataraz = razasdata.json()
+    print('Elija su raza de las presentadas a continuación: ')
+    for e in dataraz['results']:
+        print(' ' + e['index'])
 
+    razaseleccionada = input('Elija su raza: ')
 
+    
 # Aqui elegiremos la clase
 
-#if data.status_code == 200:
-#   data = data.json()
-#    for e in data['results']:
-#        resultado = e + e['index']
-#        print (resultado)
+print('****************************')
+if clasesdata.status_code == 200:
+    dataclas = clasesdata.json()
+    print('Elija una clase de las presentadas a continuación')
+    for i in dataclas['results']:
+        print(' ' + i['index'])
+    
+    
+    claseseleccionada = input('Elija su clase: ')
+    
+# Output de la clase
+print('Su personaje es: ', nombre," el ", claseseleccionada, " ", razaseleccionada )     
+
+# Aqui calcularemos las estadisticas del personaje 
+# Se calculará siguiente un sistema de point buy
+# Es decir, se te asigna un total de puntos
+# con el cual comprar el valor de tus estadisticas
+
+totalPuntos = 27
+caracteristicaInicial = 8
+fuerza = caracteristicaInicial
+destreza = caracteristicaInicial
+constitución = caracteristicaInicial
+inteligencia = caracteristicaInicial
+sabiduria = caracteristicaInicial
+carisma = caracteristicaInicial  
+
+
+
+while totalPuntos != 0:
+    print("Contador:", totalPuntos)
+    totalPuntos -= 1
+
+print("Te has quedado sin puntos")
